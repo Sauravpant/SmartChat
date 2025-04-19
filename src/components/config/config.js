ccaee2cff9168863a7345f6f69a575c517345255
Saurav Pant <sauravpant777@gmail.com>
2025-04-19 12:50:28 +0545

    Setup Gemini API

---
+++ src/components/config/config.js
@@ -0,0 +1,13 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY });
+
+async function Chat(prompt) {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: prompt,
+  });
+  console.log(response.text);
+  return response.text;
+}
+export default Chat;
